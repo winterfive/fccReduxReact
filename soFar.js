2579fb6c0fd9a850351c78ade85f9c05e1f8f2f4
Lee <winterfive@users.noreply.github.com>
2019-02-11 14:07:35 -0500

    Added connect @186

--- soFar.js
+++ soFar.js
@@ -182,3 +182,38 @@ const mapDispatchToProps = (dispatch) => {
     }
     return newObj;
 }
+
+// ADDING CONNECT
+
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {
+    messages: state
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (message) => {
+      dispatch(addMessage(message));
+    }
+  }
+};
+
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h3>This is a Presentational Component</h3>
+  }
+};
+
+const connect = ReactRedux.connect;
+const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(Presentational)
